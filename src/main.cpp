476cc3db7a6c6d11b557de1474ae37a488e33eed
Juan Medina <jamedina@gmail.com>
2025-12-28 20:56:30 +0000

    Integrate spdlog as a submodule and update main
    Added spdlog as a git submodule and updated CMakeLists.txt to include and link spdlog as a header-only library. Modified main.cpp to use spdlog for logging. Also renamed clang-format to .clang-format and made a minor formatting change.

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,19 @@
 
 #include <cstdio>
 #include <iostream>
+#include <spdlog/spdlog.h>
 
 [[nodiscard]] auto main(int /*argc*/, char * /*argv*/[]) -> int {
-  try {
-    std::cout << "Hello, Energy Swap!" << '\n';
-    return EXIT_SUCCESS;
-  } catch (const std::exception &e) {
-    std::fputs("Unhandled exception in main: ", stderr);
-    std::fputs(e.what(), stderr);
-    std::fputc('\n', stderr);
-  } catch (...) {
-    std::fputs("Unhandled non-standard exception in main\n", stderr);
-  }
-  return EXIT_FAILURE;
+    try {
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] [%@] %v");
+        SPDLOG_INFO("Hello from spdlog!");
+        return EXIT_SUCCESS;
+    } catch(const std::exception &e) {
+        std::fputs("Unhandled exception in main: ", stderr);
+        std::fputs(e.what(), stderr);
+        std::fputc('\n', stderr);
+    } catch(...) {
+        std::fputs("Unhandled non-standard exception in main\n", stderr);
+    }
+    return EXIT_FAILURE;
 }
